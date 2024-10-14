472c02e7a473c56ce8b6de1da15bad153e240e8c
nott <isnott1028@outlook.com>
2024-10-14 21:59:13 +0800

    first commit

---
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -0,0 +1,14 @@
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"SimpleWorld\",\"description\":\"一款基于Towny的社会模拟服务器\",\"head\":[],\"locales\":{}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
---
+++ docs/.vuepress/.temp/pages/join.html.vue
@@ -0,0 +1,10 @@
+<template><div><h1 id="加入我们" tabindex="-1"><a class="header-anchor" href="#加入我们"><span>加入我们</span></a></h1>
+<h2 id="qq交流群" tabindex="-1"><a class="header-anchor" href="#qq交流群"><span>QQ交流群</span></a></h2>
+<p>960431068</p>
+<h2 id="qq频道" tabindex="-1"><a class="header-anchor" href="#qq频道"><span>QQ频道</span></a></h2>
+<p>SimpleWorld</p>
+<h2 id="kook频道" tabindex="-1"><a class="header-anchor" href="#kook频道"><span>kook频道</span></a></h2>
+<p>暂无</p>
+</div></template>
+
+
